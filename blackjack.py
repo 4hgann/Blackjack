87deaa2d047fdce2aeb55765be3592fc42fd5e1d
4hgann <henry.gann@gmail.com>
2021-10-13 09:18:22 +1300

    Added formatting for games print statements.

--- blackjack.py
+++ blackjack.py
@@ -26,7 +26,7 @@ def compare(house,user,bet):
 def houseDraw(house,deck):
     print("The house started with: ",end="")
     house.printHand()
-    input('')
+    input('(Enter to continue...)')
     while(house.totalValue() < 17):
         house.addCard(deck.draw())
         print("The house draws and their new hand is: ",end="")
@@ -76,7 +76,7 @@ def playRound(house,user):
     bet = getBet(user)
     pot=2*bet
 
-    os.system('cls||clear')
+
     print('The pot is: $' +str(pot))
 
     #house.printHand() Uncomment to see the dealers entire hand
@@ -99,6 +99,7 @@ def playRound(house,user):
             houseDraw(house,freshDeck)
             if(house.totalValue() > 21):
                 print('The house went bust, you win!')
+                userWin(house,user,bet)
                 break
             else:
                 compare(house,user, bet)
@@ -119,11 +120,13 @@ if __name__ == "__main__":
     while True:
         if(user.chips <= 0):
             print('You went broke')
+            input('Press enter to close the game')
             break
-
+        
         print('You currently have: $' + str(user.chips))
         choice = input('Do you want to play another round? Yes (Y) or No (N)')
         os.system('cls||clear')
+        print('You currently have: $' + str(user.chips))
         if(choice.lower()=='no' or choice.lower()=='n'):
             print('You cashed out with $' + str(user.chips))
             break
